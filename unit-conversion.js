b2cfc80148c40c8ce168ff8cac4e4d42141163bc
Justin <jstnbphillips@gmail.com>
2018-08-02 08:01:53 -0700

    Planning...

---
+++ unit-conversion.js
@@ -0,0 +1,22 @@
+//united states customary units of length
+    // international
+    const lengthPoint = 1; // p
+    const lengthPica = 12; // P/
+    const lengthInch = 72; // in
+    const lengthFoot = 864; // ft
+    const lengthYard = 2592; // yd
+    const lengthMile = 1760 * lengthYard; // mi
+    // us survey
+    const lengthLink = 7.92 * lengthInch; // li
+    const lengthRod = 25 * lengthLink; // rd
+    const lengthChain = 4 * lengthRod; // ch
+    const lengthFurlong = 10 * lengthChain; // fur
+    const lengthStatuteMile = 8 * lengthFurlong; // mi
+    const lengthLeague = 3 * lengthStatuteMile; // lea
+    // international nautical
+    const lengthFathom = 2 * lengthYard; // ftm
+    const lengthCable = 120 * lengthFathom; // cb
+    const lengthNauticalMile = 8.439 * lengthCable; // NM or nmi
+
+
+    console.log(lengthYard);
