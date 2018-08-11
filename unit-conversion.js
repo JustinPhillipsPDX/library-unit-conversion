b7eb8be9d89bc05d5d1e729cf427d2774871b839
Justin <jstnbphillips@gmail.com>
2018-08-10 21:47:36 -0700

    Added more units

--- unit-conversion.js
+++ unit-conversion.js
@@ -19,6 +19,7 @@ const lengthCable = 622080; // cb
 const lengthNauticalMile = 5249733.12; // NM or nmi
 
 // metric units of length
+const lengthNanometer = 2.8346e-6; // nm
 const lengthMillimeter = 2.83465; // mm
 const lengthCentimeter = 28.3465; // cm
 const lengthDecimeter = 283.465; // dm
@@ -26,11 +27,19 @@ const lengthMeter = 2834.65; // m
 const lengthDekameter = 28346.5; // dam
 const lengthHectometer = 283465; // hm
 const lengthKilometer = 2834650; // km
+const lengthGigameter = 2.8346457e+12; // Gm
+
+// astronomical units of length
+const lengthEarthRadius = 18060641280; // RE
+const lengthLunarDistance = 1089641963520; // LD
+const lengthAstronomicalUnit = 4.2405696e+14; // AU
+const lengthLightYear = 7.76018152e-7; // ly
+const lengthParsec = 8.7468026e+19; // pc
+const lengthHubbleLength = 3.700858975415859e+29; // c/H₀
 
 //
 function convert(value,fromUnit,toUnit) {
     return (value * fromUnit / toUnit);//.toFixed(10);
 }
 
-//console.log("Convert 1 Foot to Millimeters: " + convert(1, lengthFoot, lengthMillimeter)); // 304.7995343340448
-//console.log("Convert 1 Millimeter to Feet: " + convert(1, lengthMillimeter, lengthFoot)); // 0.0032808449074074072
+//console.log(convert(1, lengthHubbleLength, lengthMile) + " miles"); // 8.112503014993379e+22 miles
