3b2defe78ac3fe7b080bcaa7689cdb45f4efdb17
Justin <jstnbphillips@gmail.com>
2018-08-10 20:49:32 -0700

    Added metric units of length

--- unit-conversion.js
+++ unit-conversion.js
@@ -1,8 +1,8 @@
-//united states customary units of length
+// united states customary units of length
 // international
 const lengthPoint = 1; // p
 const lengthPica = 12; // P/
-const lengthInch = 72; // in
+const lengthInch = 72; // in 0.0138889
 const lengthFoot = 864; // ft
 const lengthYard = 2592; // yd
 const lengthMile = 4561920; // mi
@@ -18,9 +18,19 @@ const lengthFathom = 5184; // ftm
 const lengthCable = 622080; // cb
 const lengthNauticalMile = 5249733.12; // NM or nmi
 
-// 
+// metric units of length
+const lengthMillimeter = 2.83465;
+const lengthCentimeter = 28.3465;
+const lengthDecimeter = 283.465;
+const lengthMeter = 2834.65;
+const lengthDekameter = 28346.5;
+const lengthHectometer = 283465;
+const lengthKilometer = 2834650;
+
+//
 function convert(value,fromUnit,toUnit) {
     return (value * fromUnit / toUnit);//.toFixed(10);
 }
 
-console.log(convert(1, lengthChain, lengthFurlongUs)); // 0.1
+//console.log("Convert 1 Foot to Millimeters: " + convert(1, lengthFoot, lengthMillimeter)); // 304.7995343340448
+//console.log("Convert 1 Millimeter to Feet: " + convert(1, lengthMillimeter, lengthFoot)); // 0.0032808449074074072
