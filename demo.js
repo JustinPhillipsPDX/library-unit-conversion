78616acc1487f283917033eccc57d580b0ed113b
Justin <jstnbphillips@gmail.com>
2018-08-14 00:58:41 -0700

    Updating keyboard click sound.

--- demo.js
+++ demo.js
@@ -3,12 +3,12 @@ $(function () {
     var clickSound = new Audio();
     clickSound.src = "./click.mp3";
     
-    $(".keyButton, .sound").mousedown(function(){
-       clickSound.play(); 
+    $(".keyButton").mousedown(function(){
+        clickSound.load();
+        clickSound.play(); 
        /*
        sound function needs to stop sound when another key is clicked
        */
     });
-
 
 });
